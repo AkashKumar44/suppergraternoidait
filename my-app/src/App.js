7851ed57d9eb4e9f4ed1d1b36f8a101f17bf7297
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2022-07-31 22:40:52 -0700

    any new file uplode and create
    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import emailjs, { init } from "@emailjs/browser";
+// import './node_modules/tailwindcss';
 
 function App() {
+  init("user_xxxxxxxxxxxxxxxxxxx");
+  const form = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm("SERVICE_D", "TEMPLAE_ID", form.current, "USER_ID").then(
+      (result) => {
+        alert("Message Sent Successfully");
+        console.log(result.text);
+      },
+      (error) => {
+        alert("Message Sent error");
+        console.log(error.text);
+      }
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="form font-bold underline" onSubmit={handleSubmit} ref={form}>
+        <h1 className="text-center">Registration Form</h1>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="First Name">First Name</label>
+            <input type="text" className="form-control" name="firstname" />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="Last Name">Last Name</label>
+            <input type="text" className="form-control" name="lastname" />
+          </div>
+          <div className="form-group col-12">
+            <label htmlFor="inputAddress">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputAddress"
+              placeholder="1234 Main St"
+              name="user_address"
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="message">message</label>
+            <textarea
+              type="text"
+              className="form-control"
+              id="inputmessage4"
+              name="user_message"
+            />
+          </div>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Sign in
+        </button>
+      </form>
     </div>
   );
 }
--- my-app/src/Deom.js
+++ my-app/src/Deom.js
@@ -1,3 +1,4 @@
+import logo from './logo.svg';
 import React, {Component} from "react";
 import { ReactDOM } from "react-dom";
 import './Demo.css';
@@ -5,8 +6,9 @@ import './Demo.css';
 
 
 const Demo = ({name}) =>{
-    return <nav className="mainstyle">
-    <h1>Hello {name}</h1>
+    return <nav className="App-header">
+    <h1 className='App'>Hello {name}</h1>
+    <img src={logo} className="App-logo" alt="logo" />
 </nav>
 }
 
